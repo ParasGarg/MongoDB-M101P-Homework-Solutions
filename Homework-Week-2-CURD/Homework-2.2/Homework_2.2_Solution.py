68d59278408a92e081e278e2f14d9ac0e204e8af
Paras Garg <garg.paras.14@gmail.com>
2017-01-27 01:07:26 -0500

    Homework 2.2 Solution

---
+++ Homework-Week-2-CURD/Homework-2.2/Homework_2.2_Solution.py
@@ -0,0 +1,65 @@
+import pymongo
+
+# establishing connection
+connection = pymongo.MongoClient("mongodb://localhost")
+
+# get a handle to database
+db = connection.students
+grades = db.grades
+
+# function to find total number of student of grade type "homework"
+def students_count():
+    print "Total number of students of grade type 'homework' :: "
+    query = { 'type':'homework' }
+
+    try:
+        cursor = grades.find(query)
+
+    except Exception as ex:
+        print "Unexpected Error", type(ex), ex
+
+    student_id_count = 0
+    for doc in cursor:
+        if student_id_count < doc['student_id']:
+            student_id_count = doc['student_id']
+
+    print student_id_count, "\n"
+    find_students_of_type_homework(student_id_count)
+
+# function to find all documents of students having grade type "homework"
+def find_students_of_type_homework(student_id_count):
+    # print "Documents of students of type 'homework' are :: "
+    for id in range(0, student_id_count+1):
+        query = { 'student_id':id, 'type':'homework' }
+
+        try:
+            cursor = grades.find(query)
+
+        except Exception as ex:
+            print "Unexpected Error", type(ex), ex
+
+        min_score = 100.0
+        min_doc_id = 0
+        for doc in cursor:
+            if min_score > doc['score']:
+                min_score = doc['score']
+                min_doc_id = doc['_id']
+
+        # print min_doc_id, "-->" id, "-->", min_score
+        delete_student_document(min_doc_id)
+        print "Deleted student_id", id
+
+# function to delete the selected student
+def delete_student_document(id):
+    # print "Document to delete";
+    query = { '_id':id }
+
+    try:
+        grades.delete_one(query)
+         
+    except Exception as ex:
+        print "Unexpected Error", type(ex), ex
+
+
+# function calls
+students_count()
